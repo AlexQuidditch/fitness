046a3feb9185d7f975b846f854788161166831d1
AlexQuidditch <alexquidditch@gmail.com>
2021-10-03 01:24:15 +0700

    remove junk

--- src/scripts/schedule.ts
+++ src/scripts/schedule.ts
@@ -1,6 +1,4 @@
-import 'tui-calendar/dist/tui-calendar.css';
-
-function formatDate(dateString: string) {
+export function formatDate(dateString: string) {
   return new Intl.DateTimeFormat('ru-RU', { weekday: 'short', month: 'long', day: 'numeric' })
     .format(new Date(dateString))
     .replaceAll(',', '\n')
